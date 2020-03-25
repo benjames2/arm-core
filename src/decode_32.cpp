a87a9867400a572a578415c285992b966de2a587
benjames2 <campbelljamesone@gmail.com>
2020-03-25 16:27:36 -0500

    finished decode_32b_A6_102_LDRD_imm

--- src/decode_32.cpp
+++ src/decode_32.cpp
@@ -173,7 +173,7 @@ instruction_32b_t decode_32b_A5_21(unsigned int PC, unsigned int instruction_wor
         else if(op2 == 0x10)
             return decode_32b_A6_230_STRD(PC, instruction_word);
         else
-            return decode_32b_A6_102_LDRD(PC, instruction_word);
+            return decode_32b_A6_102_LDRD_imm(PC, instruction_word);
     }
 
     else if(op1 == 0x01){
@@ -208,7 +208,7 @@ instruction_32b_t decode_32b_A5_21(unsigned int PC, unsigned int instruction_wor
             return decode_32b_A6_230_STRD(PC, instruction_word);
         }
         else{
-            return decode_32b_A6_102_LDRD(PC, instruction_word);
+            return decode_32b_A6_102_LDRD_imm(PC, instruction_word);
         }
     }
 
@@ -363,14 +363,26 @@ instruction_32b_t decode_32b_A6_230_STRD(unsigned PC, unsigned int instruction_w
 }
 
 
-instruction_32b_t decode_32b_A6_102_LDRD(unsigned PC, unsigned int instruction_word){
+instruction_32b_t decode_32b_A6_102_LDRD_imm(unsigned PC, unsigned int instruction_word){
+    
     instruction_32b_t in;
+
+    in.P = (instruction_word >> (15 + 9)) & 0x01;
+    in.U = (instruction_word >> (15 + 8)) & 0x01;
+    in.W = (instruction_word >> (15 + 6)) & 0x01;
+    in.Rn = (instruction_word >>(15 +1)) & 0x0F;
+    in.Rt = (instruction_word >> 12) & 0x0F;
+    in.Rt2 = (instruction_word >> 8) & 0x0F;
+    in.u32 = (instruction_word >> 0) & 0xFF;
 
     return in;
 }
 
 instruction_32b_t decode_32b_A6_233_STREXB(unsigned PC, unsigned int instruction_word){
+    
     instruction_32b_t in;
+
+
 
     return in;
 }
