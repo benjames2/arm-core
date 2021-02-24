42b856c2fa42cd4abaa8abc1151d09b6ba6444ce
benjames2 <campbelljamesone@gmail.com>
2021-02-23 19:36:21 -0600

    execute is now included in the merged. Forgo to save it previously

--- inc/execute.h
+++ inc/execute.h
@@ -7,10 +7,6 @@
 #include "core.h"
 #include "memory_pool.h"
 #include "decode_structure.h"
-<<<<<<< HEAD
-#include <inc/decode.h>
-=======
 #include "decode.h"
->>>>>>> master
 
 armstate_t execute(armstate_t& armstate, decoded_instruction_t& inst);
